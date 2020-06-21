ec8120c0c7c8155d3d1b03cbdc0138177fdf81cb
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-06-21 15:29:05 -0300

    Criando form sem formik e yup

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './styles.css';
+// import {Container, ContainerForm, TextFromInput} from './styles';
 
 const Home = () => {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = () => {
+    console.log("Ok");
+    alert(`Nome: ${name}`);
+  }
+
   return(
-    <h1>Home</h1>
+    <div className="wrapper">
+        <div className="form-wrapper">
+          <h1>Create Account</h1>
+          <form onSubmit={handleSubmit} >
+            <div className="firstName">
+              <label htmlFor="firstName">First Name</label>
+              <input
+                placeholder="First Name"
+                type="text"
+                name="firstName"
+                onChange={(name) => setName(name.target.value)}
+                value={name}
+                noValidate
+              />
+            </div>
+            <div className="lastName">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                placeholder="Last Name"
+                type="text"
+                name="lastName"
+                onChange={(lastName) => setLastName(lastName.target.value)}
+                value={lastName}
+                noValidate
+              />
+            </div>
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input
+                placeholder="Email"
+                type="email"
+                name="email"
+                onChange={(email) => setEmail(email.target.value)}
+                value={email}
+                noValidate
+              />
+            </div>
+            <div className="password">
+              <label htmlFor="password">Password</label>
+              <input
+                placeholder="Password"
+                type="password"
+                name="password"
+                onChange={(password) => setPassword(password.target.value)}
+                value={password}
+                noValidate
+              />
+            </div>
+            <div className="createAccount">
+              <button type="submit">Create Account</button>
+              <small>Already Have an Account?</small>
+            </div>
+          </form>
+        </div>
+      </div>
   );
 }
 
